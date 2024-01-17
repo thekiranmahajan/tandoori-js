db17d54174bca76cf2a5e28a789884d5b03312f5
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-17 16:34:12 +0530

    style: fixed  thecard arrangement using flex

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import RestaurantCard from "./RestaurantCard";
 const RestaurantList = ({ restaurants }) => {
   return (
-    <div className="mt-10">
+    <div className="mt-10 flex flex-wrap justify-center">
       {restaurants.map((restaurant) => (
         <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
       ))}
