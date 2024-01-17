f2683bd0a882d9adf9842e7c63d82d4f41df4c5b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-17 16:14:02 +0530

    style: fixed the width of left side of Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <img className="h-16" src={logo} alt="TandooriJs_Logo" />
         <h2 className="font-[Pacifico] text-2xl">TandooriJs.</h2>
       </div>
-      <div className="flex items-center justify-around h-full w-1/3 font-semibold font-[Montserrat] ">
+      <div className="flex items-center justify-around h-full max-w-[50%]   font-semibold font-[Montserrat] p-10 gap-10">
         <a href="#">Home</a>
         <a href="#">Search</a>
         <a href="#">About</a>
