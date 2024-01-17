9864131b951db65651761c633e5d172cee748d27
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-17 16:53:07 +0530

    feat: working on Carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Carousel = () => {
+  return <div className="w-full ">
+<div className="slider">
+    hello
+</div>
+
+  </div>;
+};
+
+export default Carousel;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 const Main = () => {
   return (
-    <div className="bg-gray-200 min-h-screen w-full">
+    <div className="bg-gray-200 min-h-screen w-full flex items-center  flex-col overflow-x-hidden">
       <Header />
+      <Body />
     </div>
   );
 };
