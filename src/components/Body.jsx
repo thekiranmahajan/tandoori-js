6c43c17f2f9525e71c2ddd264b77150f43f8366f
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-17 16:22:01 +0530

    feat: implemented useEffect hook to efficient data retrieval from a dynamic fetch API call

---
+++ src/components/Body.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Carousel from "./Carousel";
+import RestaurantList from "./RestaurantList";
+const Body = () => {
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const getRestaurants = async () => {
+    try {
+      const response = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.51981990724166&lng=73.86026275822753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const data = await response.json();
+      console.log(
+        data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setAllRestaurants(
+        data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+    } catch (error) {
+      console.log("Something went wrong while fetching API...😵");
+    }
+  };
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  return (
+    <div className="w-11/12 min-h-[80vh]  flex items-center flex-col">
+      {/* <Carousel /> */}
+      <RestaurantList restaurants={allRestaurants} />
+    </div>
+  );
+};
+
+export default Body;
