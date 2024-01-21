bf6aa4c61bd66757f05e75179764255a438a9527
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-21 09:30:42 +0530

    fixed: no return from filter() method of onSearch

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -16,38 +16,30 @@ const Body = () => {
         encodeURIComponent(
           "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.51981990724166&lng=73.86026275822753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
+
       const response = await fetch(url);
       const data = await response.json();
-      // console.log(
-      //   data?.data?.cards[0]?.card?.card
-      // );
-      if (data?.data?.cards[1]) {
-        setAllRestaurants(
-          data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-            ?.restaurants
-        );
-        setCarouselCards(
-          data?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info
-        );
-      } else {
-        setAllRestaurants(
-          data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
-            ?.restaurants
-        );
-      }
+
+      setAllRestaurants(
+        data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setCarouselCards(
+        data?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info
+      );
     } catch (error) {
       console.log("Something went wrong while fetching API...😵");
     }
   };
 
   const onSearch = (searchText) => {
-    const filteredData = allRestaurants.filter((restaurant) => {
-      restaurant.info.name.toLowerCase().includes(searchText.toLowerCase());
-      console.log(searchText, restaurant.info.name);
-    });
+    const filteredData = allRestaurants.filter((restaurant) =>
+      restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())
+    );
     setFilteredRestaurants(filteredData);
+    console.log(filteredData);
   };
-  console.log(filteredRestaurants);
+
   useEffect(() => {
     getRestaurants();
   }, []);
@@ -59,10 +51,13 @@ const Body = () => {
       <SearchBar
         searchText={searchText}
         setSearchText={setSearchText}
-        onSearch={() => onSearch(searchText)}
+        onSearch={onSearch}
       />
-
-      <RestaurantList restaurants={filteredRestaurants} />
+      {filteredRestaurants.length <= 0 ? (
+        <RestaurantList restaurants={allRestaurants} />
+      ) : (
+        <RestaurantList restaurants={filteredRestaurants} />
+      )}
     </div>
   );
 };
