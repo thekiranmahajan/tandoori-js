e3146dcf2ef84229857e75220d470da916c6714b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-21 14:37:07 +0530

    feat: added Footer component and renamed main.js to app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 const Main = () => {
   return (
     <div className="bg-gray-200 min-h-screen w-full flex items-center  flex-col overflow-x-hidden">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className="h-20 w-full flex items-center justify-center shadow-md bg-gray-300">
+      ©️ Copyright {currentYear}. Kiran Mahajan. All Rights Reserved
+    </div>
+  );
+};
+
+export default Footer;
