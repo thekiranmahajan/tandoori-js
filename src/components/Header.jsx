f6c00aecdb28066e5bf079e36b8743a3d2e76162
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-22 17:33:56 +0530

    feat: integrated the responsive Header with hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import logo from "../../public/images/logo.png";
+import NavLinks from "./NavLinks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBurger, faXmark } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className="h-20 w-full flex items-center justify-between bg-white px-8 shadow-md">
+    <div className="relative h-20 w-full flex items-center justify-between bg-white px-8 shadow-md">
       <div className="flex items-center">
         <img className="h-16" src={logo} alt="TandooriJs_Logo" />
-        <h2 className="font-[Pacifico] text-2xl">TandooriJs.</h2>
+        <h2 className="font-[Pacifico] text-2xl">Tandoori Js.</h2>
       </div>
+      <NavLinks className="hidden md:flex items-center justify-around h-full max-w-[50%] font-semibold font-[Montserrat] p-10 gap-10" />
+      <button
+        onClick={toggleMenu}
+        className="text-3xl transition-all duration-300 md:hidden"
+      >
+        <FontAwesomeIcon icon={isOpen ? faBurger : faXmark} />
+      </button>
 
-      <div className="flex items-center justify-around h-full max-w-[50%]   font-semibold font-[Montserrat] p-10 gap-10">
-        <a href="#">Home</a>
-        <a href="#">Search</a>
-        <a href="#">About</a>
-        <a href="#">Cart</a>
-        <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
-          {isLoggedIn ? "Logout" : "Login"}
-        </button>
-      </div>
+      {!isOpen && (
+        <NavLinks className=" bg-gray-800 rounded-lg backdrop-filter backdrop-blur-sm bg-opacity-30 w-11/12 h-52  flex flex-wrap items-center justify-center flex-col md:hidden z-10  absolute m-auto left-0 right-0 top-24 text-xl gap-2" />
+      )}
     </div>
   );
 };
