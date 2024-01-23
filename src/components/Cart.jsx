23f4f18844f84b5c7aaa00b5cf1555286d331b31
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-23 13:50:27 +0530

    feat: created route pages as components

---
+++ src/components/Cart.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Cart = () => {
+  return <div>Cart page</div>;
+};
+export default Cart;
---
+++ src/components/Search.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Search = () => {
+  return <div>Search Page</div>;
+};
+export default Search;
