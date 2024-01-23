798d701517481e715b68be75b01f9b9e52914842
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-23 12:12:47 +0530

    feat: configured the NavLink component provided by the react-router-dom with additional functionalities such as isActive for w.r.t route

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,15 +1,43 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavLinks = ({ className }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className={className}>
-      <Link to="/">Home</Link>
-      <Link to="/search">Search</Link>
-      <Link to="/about">About</Link>
-      <Link to="/cart">Cart</Link>
+      <NavLink
+        to="/"
+        className={({ isActive }) => {
+          return isActive ? "active-navlink" : "";
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/search"
+        className={({ isActive }) => {
+          return isActive ? "active-navlink" : "";
+        }}
+      >
+        Search
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) => {
+          return isActive ? "active-navlink" : "";
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/cart"
+        className={({ isActive }) => {
+          return isActive ? "active-navlink" : "";
+        }}
+      >
+        Cart
+      </NavLink>
       <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
         {isLoggedIn ? "Logout" : "Login"}
       </button>
