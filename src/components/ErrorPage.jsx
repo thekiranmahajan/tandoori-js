9e4faab30db8c1ac9a7584c3d281d087c6dd37a8
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-23 14:36:03 +0530

    feat: dynamically fetched values present in error object provided by useRouteError hook from react-router-dom using destructuring

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import error404 from "../../public/images/error404.svg";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const { error, status, statusText } = useRouteError();
+
   return (
-    <div className="flex items-center justify-center flex-col mt-10 h-full w-full">
+    <div className="flex items-center justify-center flex-col mt-10 h-full w-full gap-5">
       <img className="w-1/4" src={error404} alt="Error 404 page not found" />
       <h3 className="text-3xl font-semibold">Oops!! Something went wrong...</h3>
+      <h3 className="text-2xl">{status + ": " + statusText}</h3>
+      <h3 className="text-2xl">{error.message}</h3>
     </div>
   );
 };
