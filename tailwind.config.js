e47d7b5161d349401fbb4f1b2817f11d2a51eebe
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-24 10:12:43 +0530

    fix: add import of external google fonts in tailwind css according to docs with additional font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Pacifico: ['"Pacifico"', ...defaultTheme.fontFamily.sans],
+        Montserrat: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+        RobotoCondenced: [
+          '"Roboto Condensed"',
+          ...defaultTheme.fontFamily.serif,
+        ],
+      },
+    },
   },
   plugins: [],
 };
