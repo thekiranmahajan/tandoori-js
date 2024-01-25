b1a251a6e1ae112183852ac068f0b63dd47dff2a
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-25 19:11:20 +0530

    feat: add GitHub link to author name in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,15 @@ import React from "react";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="h-20 w-full flex items-center justify-center shadow-md bg-gray-300   text-center">
-      ©️ Copyright {currentYear}. Kiran Mahajan. All Rights Reserved
+    <div className="h-20 w-full flex items-center justify-center shadow-md bg-gray-300  font-semibold text-center gap-2">
+      <span> © {currentYear} All Rights Reserved </span>
+      <a
+        href="https://github.com/thekiranmahajan"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Kiran Mahajan
+      </a>
     </div>
   );
 };
