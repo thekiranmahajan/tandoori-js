14fab454dd25f15ce99e05453b13963d6a6f01a1
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-30 22:27:25 +0530

    feat: work on basic styling of About page and img imports

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import aboutImg from "../../public/images/aboutImg.svg";
 
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <div className="h-screen w-full pt-20 flex items-center flex-col">
+      <h1>About-Us</h1>
+      <img
+        className="h-96 bg-blend-multiply"
+        src={aboutImg}
+        alt="about image"
+      />
+    </div>
+  );
 };
 export default About;
