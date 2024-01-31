7e4c7a3e075a6e9650e2a8a834618748ad22a9a0
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-01-31 22:52:55 +0530

    feat: working on About page added logo of tech stacks used with styling

--- src/constants.js
+++ src/constants.js
@@ -8,9 +8,17 @@ export const API_URL =
   encodeURIComponent(
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.51981990724166&lng=73.86026275822753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
   );
-  
+
 export const MENU_API =
   "https://corsproxy.org/?" +
   encodeURIComponent(
     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.51981990724166&lng=73.86026275822753&restaurantId="
   );
+
+export const PARCEL_LOGO = "https://parceljs.org/avatar.b1be591d.avif";
+
+export const REACT_LOGO =
+  "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg";
+
+export const TAILWINDCSS_LOGO =
+  "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg";
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import aboutImg from "../../public/images/aboutImg.svg";
+import { REACT_LOGO, PARCEL_LOGO, TAILWINDCSS_LOGO } from "../constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFontAwesome } from "@fortawesome/free-brands-svg-icons";
+import REACT_ROUTER_LOGO from "../../public/images/REACT_ROUTER_LOGO.svg";
 
 const About = () => {
   return (
-    <div className="h-screen w-full pt-20 flex items-center flex-col">
-      <h1>About-Us</h1>
+    <div className="h-screen w-11/12 pt-24 flex items-center flex-col">
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p className=" w-full font-semibold mt-5 text-wrap p-2 text-center text-gray-800">
+        Tandoori Js - Reacting to Your Hunger, One Byte at a Time! 🌶️
+      </p>
+      <p className="w-full flex items-center justify-center font-semibold text-sm mt-1 gap-4 flex-wrap">
+        Powered by{" "}
+        <img className="h-6 ml-4" src={PARCEL_LOGO} alt="PARCEL_LOGO" /> +
+        <img className="h-6" src={REACT_LOGO} alt="REACT_LOGO" /> +
+        <img className="h-5" src={REACT_ROUTER_LOGO} alt="REACT_ROUTER_LOGO" />
+        +
+        <img className="h-5" src={TAILWINDCSS_LOGO} alt="TAILWINDCSS_LOGO" /> +
+        <FontAwesomeIcon className="text-2xl" icon={faFontAwesome} />
+      </p>
       <img
         className="h-96 bg-blend-multiply"
         src={aboutImg}
         alt="about image"
       />
+      <p className="text-sm text-gray-500">
+        Welcome to Tandoori Js, a culinary haven where cutting-edge technology
+        meets a world of flavors. At Tandoori Js, we go beyond being just a food
+        delivery platform; we're a celebration of innovation, taste, and the
+        seamless integration of technology into the art of dining.
+      </p>
     </div>
   );
 };
