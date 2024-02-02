0100c3626d124732c29b0e14c9d676aa576bec19
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-02 20:20:27 +0530

    style: create the basic UI for searchbar

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Search = () => {
-  return <div className="pt-16">Search Page</div>;
+  return (
+    <div className="min-h-screen w-3/4 flex items-center flex-col pt-28 mb-24">
+      <input
+        className="relative truncate w-3/4 h-12 rounded-lg py-2 px-6 outline-none"
+        type="text"
+        placeholder="Search for restaurants to fulfill the bar of hunger 🍟..."
+      />
+    </div>
+  );
 };
 export default Search;
