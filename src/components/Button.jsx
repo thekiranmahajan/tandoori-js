40d3b232585bd690ce9a6f7d0f62a1b1abba0494
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-03 21:51:04 +0530

    feat: add icon with dynamic appearance based on onFocus and onBlur with a reused Button component for user instruction

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-const Button = ({ onClick = () => {}, btnText, btnIcon }) => {
+const Button = ({
+  onClick = () => {},
+  btnText,
+  btnIcon = "",
+  className = "",
+}) => {
   return (
     <button
       onClick={onClick}
-      className="px-3 py-2 bg-[#1c1c21] text-white rounded-lg  focus:ring-4 ring-yellow-300 transition-transform hover:scale-95 focus:scale-100 cursor-pointer text-sm font-semibold text-center flex items-center gap-1"
+      className={`px-3 py-2 bg-[#1c1c21] text-white rounded-lg  focus:ring-4 ring-yellow-300 transition-transform hover:scale-95 focus:scale-100 cursor-pointer text-sm font-semibold text-center flex items-center gap-1 ${className}`}
     >
       {btnIcon && <FontAwesomeIcon icon={btnIcon} />}
       {btnText}
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import Button from "../components/Button";
 
 const Search = () => {
+  const [isTyping, setIsTyping] = useState(false);
   return (
     <div className="min-h-screen w-3/4 flex items-center flex-col pt-28 mb-24">
-      <input
-        className="relative truncate w-3/4 h-12 rounded-lg py-2 px-6 outline-none"
-        type="text"
-        placeholder="Search for restaurants to fulfill the bar of hunger 🍟..."
-      />
+      <div className="relative w-3/4 h-12 flex items-center">
+        <input
+          onFocus={() => setIsTyping(!isTyping)}
+          onBlur={() => setIsTyping(!isTyping)}
+          className="truncate w-full h-12 rounded-lg py-2 pl-6 pr-20 md:pr-24 outline-none focus:ring-4 ring-yellow-300"
+          type="text"
+          placeholder="Search for restaurants to fulfill the bar of hunger 🍟..."
+        />
+        <FontAwesomeIcon
+          className={isTyping ? "hidden" : "absolute right-4 text-xl"}
+          icon={faMagnifyingGlass}
+        />
+        <Button
+          btnText={"Enter"}
+          className={
+            !isTyping
+              ? "hidden"
+              : "absolute font-semibold right-3 md:right-5 text-xs bg-[#1c1c24] text-white rounded-md p-2 "
+          }
+        />
+      </div>
     </div>
   );
 };
