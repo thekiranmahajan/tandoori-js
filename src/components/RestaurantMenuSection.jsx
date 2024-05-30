b69ae9bcd040d1aef0e43dbdd28a52aea5d62f29
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-05-30 21:45:44 +0530

    feat: filter section based on itemCards obj an map  each section to render accordian of menu item list

--- src/components/RestaurantMenuSection.jsx
+++ src/components/RestaurantMenuSection.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { IMG_URL } from "../utils/constants";
 
-const RestaurantMenuSection = () => {
-  return <div>RestaurantMenuSection</div>;
+const RestaurantMenuSection = ({ title, itemCards }) => {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <div className="mt-2 flex w-full flex-col gap-2 rounded-xl  p-2  font-semibold">
+      <div
+        onClick={() => setIsVisible((prev) => !prev)}
+        className="mb-3 flex h-12 w-full items-center justify-between pl-10 pr-5"
+      >
+        <h2 className="text-lg font-bold">
+          {title} <span>({itemCards?.length})</span>
+        </h2>
+        <FontAwesomeIcon
+          className={`cursor-pointer text-xl transition-all duration-300 ${isVisible ? "rotate-180" : "rotate-0"}`}
+          icon={faAngleUp}
+        />
+      </div>
+
+      <div
+        className={`transition-all duration-300 ${isVisible ? "block opacity-100" : "hidden opacity-0"}`}
+      >
+        {itemCards?.map((itemCard, i) => (
+          <div
+            key={itemCard?.card?.info?.id}
+            className="flex justify-between border-b-2 border-gray-400 px-10 py-4"
+          >
+            <div>p</div>
+            <div>
+              <img
+                className="h-40 w-40 rounded-xl"
+                src={IMG_URL + itemCard?.card?.info?.imageId}
+                alt={itemCard?.card?.info?.name}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantMenuSection;
--- src/pages/RestaurantMenu.jsx
+++ src/pages/RestaurantMenu.jsx
@@ -4,27 +4,23 @@ import { RestaurantMenuShimmerUI } from "../shimmers";
 import useRestaurantMenu from "../hooks/useRestaurantMenu.js";
 import { RestaurantMenuHeader } from "../components";
 import { useDispatch } from "react-redux";
-import { addItem } from "../features/cart/cartSlice";
 import floralDesign from "../../public/images/floralDesign.svg";
+import RestaurantMenuSection from "../components/RestaurantMenuSection.jsx";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
-  const { restaurantInfo, restaurantOffers } = useRestaurantMenu(resId);
-  const dispatch = useDispatch();
-
-  const handleAddItem = () => {
-    dispatch(addItem("mango"));
-  };
+  const { restaurantInfo, restaurantOffers, restaurantMenuList } =
+    useRestaurantMenu(resId);
 
   return restaurantInfo === null ? (
     <RestaurantMenuShimmerUI />
   ) : (
-    <div className="mt-8 flex min-h-screen w-4/5 flex-col items-center pt-16 md:w-2/3">
+    <div className="mb-10 mt-8 flex min-h-screen w-4/5 select-none flex-col items-center pt-16 md:w-2/3">
       <RestaurantMenuHeader
         restaurantInfo={restaurantInfo}
         restaurantOffers={restaurantOffers}
       />
-      <div className="mt-1 flex w-full items-center justify-center gap-2 rounded-xl bg-gray-100 p-2 font-semibold ">
+      <div className="mt-5 flex w-full items-center justify-center gap-2 rounded-xl bg-gray-100 p-2 font-semibold ">
         <img className="h-8 " src={floralDesign} alt="Floral Design" />
         MENU
         <img
@@ -33,7 +29,17 @@ const RestaurantMenu = () => {
           alt="Floral Design"
         />
       </div>
-      
+      {restaurantMenuList
+        ?.filter((section) => section?.card?.card?.itemCards)
+        ?.map((filteredSection, index) => (
+          <>
+            <RestaurantMenuSection
+              key={index}
+              {...filteredSection?.card?.card}
+            />
+            <div className="h-3 w-full rounded-lg bg-gray-100"></div>
+          </>
+        ))}
     </div>
   );
 };
