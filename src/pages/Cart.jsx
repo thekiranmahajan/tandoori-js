02c2a57c94ae0f31f9265216f70682312df63310
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-20 19:36:28 +0530

    feat: add empty cart page look

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { CARD_URL, IMG_URL } from "../utils/constants";
 
 const Cart = () => {
   return (
-    <div className="mb-24 flex min-h-screen w-3/4 flex-col items-center pt-28">
-      Cart page
+    <div className="flex min-h-screen w-3/4 flex-col items-center justify-center pt-28">
+      <img
+        className="w-96 mix-blend-multiply"
+        src={IMG_URL + "cart_empty_ipmau1"}
+        alt="empty cart"
+      />
+      <h3 className="mt-4 text-lg font-bold text-gray-700 md:text-xl">
+        Your cart is empty
+      </h3>
+
+      <p className="mt-1 text-center text-xs text-gray-700">
+        You can go to home page to view more restaurants
+      </p>
     </div>
   );
 };
